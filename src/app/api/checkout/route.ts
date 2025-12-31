ef8ef46ec0b5947c5847c6f28daeff4d3c59ec83
DizzyWizzy88 <153007566+DizzyWizzy88@users.noreply.github.com>
2025-12-30 21:30:40 -0600

    fix: actually update api version

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-apiVersion: null as any,
+apiVersion: "2024-11-20.acacia"  as any,
 });
 
 export async function POST(req: Request) {
