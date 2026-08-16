40ed1aa161708610ad80127d43b8e042b7087ae9
DizzyWizzy88 <dr3930397@gmail.com>
2026-08-16 11:55:00 -0500

    deps: add dotenv to server dependencies

--- server/server.js
+++ server/server.js
@@ -2,6 +2,9 @@ import express from 'express';
 import cors from 'cors';
 import admin from 'firebase-admin';
 import { GoogleGenAI } from '@google/genai';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 
